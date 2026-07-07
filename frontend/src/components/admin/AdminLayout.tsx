2b76a4e2ac60aae266c0de1169501dfa3a34b38b
Enrique Vega <enrique.quipuzcoa@gmail.com>
2026-07-07 09:36:18 -0300

    se agrega estilo para scroll

--- frontend/src/components/admin/AdminLayout.tsx
+++ frontend/src/components/admin/AdminLayout.tsx
@@ -193,7 +193,7 @@ export function AdminLayout() {
           />
         </div>
 
-        <main className="flex-1 overflow-y-auto p-6 md:p-8">
+        <main className="flex-1 overflow-y-auto p-6 md:p-8 min-h-0">
           <Outlet />
         </main>
       </div>
